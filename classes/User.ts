d8f4a66f88e658846f5ed642af85e50084e95ed3
Grapeka <chotwilaiwan@gmail.com>
2022-11-14 15:09:16 +0700

    feat: add password field for user

--- classes/User.ts
+++ classes/User.ts
@@ -2,11 +2,13 @@ abstract class User {
   protected userId: String;
   protected role: String;
   protected email: String;
+  protected password: String;
 
-  constructor(userId: String, role: String, email: String) {
+  constructor(userId: String, role: String, email: String, password: String) {
     this.userId = userId;
     this.role = role;
     this.email = email;
+    this.password = password;
   }
 
   public introduceUser() {
