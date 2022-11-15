e65c6f033cd2e9c4f5119310a535987a48b74f25
melon <momobp114@gmail.com>
2022-11-16 02:03:33 +0700

    feat: add salt to User class

--- classes/User.ts
+++ classes/User.ts
@@ -2,21 +2,19 @@ import MediaLink from "./MediaLink";
 
 abstract class User {
   mediaLink: MediaLink[] | null;
-  userId: String;
   role: String;
   email: String;
   password: String;
+  salt: String;
   profilePicture: String | null;
 
   constructor(
-    userId: String,
     role: String,
     email: String,
     password: String,
     mediaLink: MediaLink[] | null,
     profilePicture: String | null
   ) {
-    this.userId = userId;
     this.role = role;
     this.email = email;
     this.password = password;
@@ -26,7 +24,7 @@ abstract class User {
 
   public introduceUser() {
     console.log(
-      `My id: ${this.userId}, my role: ${this.role}, and my email ${this.email}`
+      `my role: ${this.role}, and my email ${this.email}`
     );
   }
 }
