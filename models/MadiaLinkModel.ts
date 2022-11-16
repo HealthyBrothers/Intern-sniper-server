e015c4d0f895a69df41970086692258307adf090
melon <momobp114@gmail.com>
2022-11-16 17:51:45 +0700

    feat: add MediaLinkModel

---
+++ models/MadiaLinkModel.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import MediaLink from "../classes/MediaLink";
+
+export interface IMediaLinkDocument extends MediaLink {}
+
+const MediaLinkSchema: mongoose.Schema = new mongoose.Schema({
+  url: { type: String, required: true },
+  type: { type: String, require: true }
+})
+
+const MediaLinkModel = mongoose.model<IMediaLinkDocument>('MediaLink', MediaLinkSchema)
+
+export default MediaLinkModel
