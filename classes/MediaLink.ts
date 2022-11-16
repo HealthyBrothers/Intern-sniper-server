cc16c91f1a5a7c79590a4ed1f411344aafc3b1df
melon <momobp114@gmail.com>
2022-11-16 17:00:17 +0700

    refactor: remove MediaLinkId

--- classes/MediaLink.ts
+++ classes/MediaLink.ts
@@ -1,12 +1,10 @@
 import { mediaLinkType } from "./enum";
 
 class MediaLink {
-  linkId: string;
   url: string;
   type: mediaLinkType;
 
-  constructor(linkId: string, url: string, type: mediaLinkType) {
-    this.linkId = linkId;
+  constructor(url: string, type: mediaLinkType) {
     this.url = url;
     this.type = type;
   }
