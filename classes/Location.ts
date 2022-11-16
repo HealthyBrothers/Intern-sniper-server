439e3978a9db57cbf0d67dc66e649689457af31e
melon <momobp114@gmail.com>
2022-11-16 19:55:32 +0700

    refactor: remove locationId

--- classes/Location.ts
+++ classes/Location.ts
@@ -1,10 +1,8 @@
 class Location {
-  locationId: String;
   country: String;
   province: String;
 
   constructor(locationId: String, country: String, province: String) {
-    this.locationId = locationId;
     this.country = country;
     this.province = province;
   }
