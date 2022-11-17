af4306840264a693a235a138b9cee0b508b28764
Grapeka <chotwilaiwan@gmail.com>
2022-11-17 16:43:48 +0700

    fix: fix import path

--- services/MediaLinkService.ts
+++ services/MediaLinkService.ts
@@ -1,16 +1,17 @@
 import MediaLink from "../classes/MediaLink";
-import MediaLinkModel, { IMediaLinkDocument } from "../models/MadiaLinkModel";
+import MediaLinkModel, { IMediaLinkDocument } from "../models/MediaLinkModel";
 
 export class MediaLinkService {
-  
-  public async create(mediaLinks: MediaLink[] | null): Promise<IMediaLinkDocument[]> {
-    const mediaLinksDocument: IMediaLinkDocument[] = []
-    if(mediaLinks !== null) {
+  public async create(
+    mediaLinks: MediaLink[] | null
+  ): Promise<IMediaLinkDocument[]> {
+    const mediaLinksDocument: IMediaLinkDocument[] = [];
+    if (mediaLinks !== null) {
       for (let mediaLink of mediaLinks) {
-        const mediaLinkDocument = await MediaLinkModel.create(mediaLink)
-        mediaLinksDocument.push(mediaLinkDocument)
+        const mediaLinkDocument = await MediaLinkModel.create(mediaLink);
+        mediaLinksDocument.push(mediaLinkDocument);
       }
     }
-    return mediaLinksDocument
+    return mediaLinksDocument;
   }
 }
