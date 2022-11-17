d353ae050d17fa581e782cb4294c9c8134bb009a
Grapeka <chotwilaiwan@gmail.com>
2022-11-17 15:43:20 +0700

    feat: export customReq

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -1,138 +1,152 @@
 import { Request, Response, NextFunction, response } from "express";
-import jwt, { JwtPayload, Secret } from 'jsonwebtoken'
-import dotenv from 'dotenv';
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import dotenv from "dotenv";
 import { UserManager } from "../classes/UserManager";
-import User from '../classes/User'
+import User from "../classes/User";
 import { MediaLinkManager } from "../classes/MediaLinkManager";
 import MediaLink from "../classes/MediaLink";
 import Location from "../classes/Location";
 
-dotenv.config()
+dotenv.config();
 
-interface CustomRequest extends Request {
-  user: User
+export interface CustomRequest extends Request {
+  user: User;
 }
 
 interface tokenizeUser {
-  email: string
+  email: string;
 }
 
-const ACCESS_TOKEN: Secret = process.env.ACCESS_TOKEN ?? ''
-const userManager = new UserManager()
-const mediaLinkManager = new MediaLinkManager()
-userManager.initialize()
+const ACCESS_TOKEN: Secret = process.env.ACCESS_TOKEN ?? "";
+const userManager = new UserManager();
+const mediaLinkManager = new MediaLinkManager();
+userManager.initialize();
 
 function generateAccessToken(user: tokenizeUser) {
   return jwt.sign(user, ACCESS_TOKEN, { expiresIn: "1800s" });
 }
 
-export function authenticateToken(req: Request, res: Response, next: NextFunction) {
+export function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) return res.sendStatus(401);
 
-  const payload = jwt.verify(
-    token,
-    ACCESS_TOKEN
-  );
-  const { email } = payload as tokenizeUser
-  const user: User | null = userManager.getUserByEmail(email)
-  if(user === null) return res.sendStatus(403);
-  (req as CustomRequest).user = user
+  const payload = jwt.verify(token, ACCESS_TOKEN);
+  const { email } = payload as tokenizeUser;
+  const user: User | null = userManager.getUserByEmail(email);
+  if (user === null) return res.sendStatus(403);
+  (req as CustomRequest).user = user;
 
-  next()
+  next();
 }
 
 export async function login(req: Request, res: Response) {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    const user = userManager.getUserByEmail(email)
-    if(user === null) return res.status(403).send('invalid email address')
-    if(! await userManager.validatePassword(user, password)) return res.status(403).send('incorrect password')
+    const user = userManager.getUserByEmail(email);
+    if (user === null) return res.status(403).send("invalid email address");
+    if (!(await userManager.validatePassword(user, password)))
+      return res.status(403).send("incorrect password");
 
-    const tokenizeUser: tokenizeUser = { email }
-    const token = generateAccessToken(tokenizeUser)
-    res.json({ token })
-  }
-  catch(err) {
-    console.error(err)
-    res.status(403)
+    const tokenizeUser: tokenizeUser = { email };
+    const token = generateAccessToken(tokenizeUser);
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(403);
   }
 }
 
 export async function registerStudent(req: Request, res: Response) {
-  const { 
+  const {
     email,
     password,
     confirmPassword,
     firstName,
     lastName,
-    studyingYear, 
-    interestedField, 
-    university, 
-    mediaLinks
-  } = req.body
+    studyingYear,
+    interestedField,
+    university,
+    mediaLinks,
+  } = req.body;
 
-  const user = userManager.getUserByEmail(email)
-  if(user !== null) {
-    return res.status(403).send('This email is already been used.')
+  const user = userManager.getUserByEmail(email);
+  if (user !== null) {
+    return res.status(403).send("This email is already been used.");
   }
-  if(password !== confirmPassword) {
-    return res.status(403).send('Password and Confirm Password doesn\'t match.')
+  if (password !== confirmPassword) {
+    return res.status(403).send("Password and Confirm Password doesn't match.");
   }
 
-  const mediaLinksParsed: MediaLink[] = mediaLinkManager.parseMediaLinks(mediaLinks)
-  userManager.createStudent({ ...req.body, mediaLink: mediaLinksParsed })
-    .then(response => {
-      res.sendStatus(201)
+  const mediaLinksParsed: MediaLink[] =
+    mediaLinkManager.parseMediaLinks(mediaLinks);
+  userManager
+    .createStudent({ ...req.body, mediaLink: mediaLinksParsed })
+    .then((response) => {
+      res.sendStatus(201);
     })
-    .catch(err => {
-      res.sendStatus(403)
-      console.error(err)
-    })
+    .catch((err) => {
+      res.sendStatus(403);
+      console.error(err);
+    });
 }
 
 export async function registerCompany(req: Request, res: Response) {
-  const { 
+  const {
     email,
     password,
     confirmPassword,
     companyName,
     phoneNumber,
     location,
-    mediaLinks
-  } = req.body
+    mediaLinks,
+  } = req.body;
 
-  const user = userManager.getUserByEmail(email)
-  if(user !== null) {
-    return res.status(403).send('This email is already been used.')
+  const user = userManager.getUserByEmail(email);
+  if (user !== null) {
+    return res.status(403).send("This email is already been used.");
   }
-  if(password !== confirmPassword) {
-    return res.status(403).send('Password and Confirm Password doesn\'t match.')
+  if (password !== confirmPassword) {
+    return res.status(403).send("Password and Confirm Password doesn't match.");
   }
 
-  let locationParsed = null
-  if(location != null) { 
-    locationParsed = new Location(location?.country, location?.province)
+  let locationParsed = null;
+  if (location != null) {
+    locationParsed = new Location(location?.country, location?.province);
   }
-  const mediaLinksParsed: MediaLink[] = mediaLinkManager.parseMediaLinks(mediaLinks)
-  userManager.createCompany({ ...req.body, mediaLink: mediaLinksParsed, location: locationParsed })
-    .then(response => {
-      res.sendStatus(201)
+  const mediaLinksParsed: MediaLink[] =
+    mediaLinkManager.parseMediaLinks(mediaLinks);
+  userManager
+    .createCompany({
+      ...req.body,
+      mediaLink: mediaLinksParsed,
+      location: locationParsed,
     })
-    .catch(err => {
-      console.error(err)
-      res.sendStatus(403)
+    .then((response) => {
+      res.sendStatus(201);
     })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(403);
+    });
 }
 export function logout(req: Request, res: Response) {
   //
 }
 
 export function me(req: Request, res: Response) {
-  const user = (req as CustomRequest).user
+  const user = (req as CustomRequest).user;
 
-  res.json({ name: user?.getName(), email: user?.email, role: user?.role, profilePicture: user?.profilePicture })
+  res.json({
+    name: user?.getName(),
+    email: user?.email,
+    role: user?.role,
+    profilePicture: user?.profilePicture,
+  });
 }
