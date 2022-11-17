5750a746429dd782770a59ae7121ae2c4ee5d073
Grapeka <chotwilaiwan@gmail.com>
2022-11-17 13:13:39 +0700

    feat: program manager methods

--- services/ProgramManager.ts
+++ services/ProgramManager.ts
@@ -1,21 +1,28 @@
 import Program from "../classes/Program";
+import Internship from "../classes/Internship";
 import ProgramModel from "../models/ProgramModel";
 import Company from "../classes/Company";
 import Timeline from "../classes/Timeline";
 
 class ProgramManager {
-  public async getProgramByName(name: String): Promise<Program[]> {
+  public async getAllPrograms(): Promise<Program[]> {
+    const programs = await ProgramModel.find();
+    return programs;
+  }
+
+  public async getProgramId(id: String): Promise<Program[]> {
     const program = await ProgramModel.find();
     return program;
   }
 
-  public async createProgram(program: Program): Promise<Program> {
-    const newProgram = await ProgramModel.create(program);
-    return newProgram;
-  }
-
-  public async updateProgram(program: Program) {
-    const updatedProgram = await ProgramModel.findOneAndUpdate;
+  public async createProgram(program: Internship): Promise<Program> {
+    if (Program instanceof Internship) {
+      const newProgram = await ProgramModel.create(program);
+      newProgram.save();
+      return newProgram;
+    } else {
+      throw new Error("Error, program doesn't a compatible type");
+    }
   }
 }
 
