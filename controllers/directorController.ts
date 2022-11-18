808511672fb144a99aa7d85d5aee08eff4f11ad6
Grapeka <chotwilaiwan@gmail.com>
2022-11-18 19:21:29 +0700

    feat : validateCompany function

--- controllers/directorController.ts
+++ controllers/directorController.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import Company from "../classes/Company";
 import { CustomRequest } from "./authController";
 import Director from "../classes/Director";
-import { UserService } from "../services/UserService";
+import { UserManager } from "../services/UserManager";
 
 dotenv.config();
 
@@ -11,11 +11,27 @@ export async function validateCompany(req: Request, res: Response) {
   console.log((req as CustomRequest).user);
 
   try {
-    if (!((req as CustomRequest).user instanceof Director)) {
+    if (!((req as CustomRequest).user.role === "Director")) {
       return res.status(403).send("You are not a director");
     }
-    const userService = new UserService();
-    const company = userService.findUserById(req.body.id);
+    const userManager = new UserManager();
+    const { id, validateStatus } = req.body;
+    const company = await userManager.findUserById(id);
+    const targetCompany = new Company(
+      company.email,
+      company.companyName,
+      null,
+      company.profilePicture,
+      company.phoneNumber,
+      null,
+      null,
+      company.password,
+      company.salt,
+      company.validateStatus
+    );
+    targetCompany.setValidateStatus(validateStatus);
+    userManager.updateUserById(id, targetCompany);
+    res.json(targetCompany);
   } catch (err) {
     console.error(err);
     res.status(403);
